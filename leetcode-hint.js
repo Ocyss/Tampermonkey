4ef20e6d7f51338a5b69b2e58fc8c11fbe21624a
Ocyss <qiudie@88.com>
2023-09-14 15:45:06 +0800

    init

---
+++ leetcode-hint.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         好好看看数据范围
+// @namespace    Ocyss
+// @version      0.1
+// @description  try to take over the world!
+// @author       Ocyss
+// @match        https://leetcode.cn/problems/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leetcode.cn
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  let t = setInterval(() => {
+    let ts = document.getElementsByName("description")[0].content.split("提示：")[1]
+    if (ts != undefined) {
+      alert("睁大你的狗眼看清楚范围：" + ts)
+      clearInterval(t)
+    }
+  }, 2000);
+})();
+
+
---
+++ universal-tool.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         universal_tool
+// @name:zh-cn   万能工具
+// @namespace    Ocyss
+// @version      0.1
+// @description  Specially written practical gadgets
+// @description:zh-cn  专门编写实用小工具
+// @author       Ocyss
+// @match        *://*/*
+// @icon         https://cdn-icons-png.flaticon.com/512/949/949339.png
+// @run-at       document-start
+// @license      MIT
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
+// ==/UserScript==
+
+
+
+(function () {
+  GM_registerMenuCommand("搜索脚本", () => {
+    GM_openInTab(`https://greasyfork.org/zh-CN/scripts/by-site/${window.location.host}?sort=updated`, {
+      active: true,
+      setParent: true
+    })
+  });
+  GM_registerMenuCommand("修改速度", () => {
+    // By: EricZhongYJ
+    let default_speed = 0.0;
+    if (document.querySelector('video').playbackRate != default_speed) {
+      default_speed = window.prompt("请输入播放速度:(Please input the speed:0-16)");
+      if (!default_speed) default_speed = document.querySelector('video').playbackRate;
+      else document.querySelector('video').playbackRate = default_speed;
+    }
+  });
+}())
