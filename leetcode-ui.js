2a4f77db3345bb1e8cab87debdaf5400dc0ab13e
Ocyss <qiudie@88.com>
2023-11-02 13:02:55 +0800

    [力扣新版评论区]-info

--- leetcode-ui.js
+++ leetcode-ui.js
@@ -4,6 +4,8 @@
 // @version      0.2
 // @description  杀软,tm的会不会设计?
 // @author       Ocyss
+// @source       https://github.com/Ocyss/Tampermonkey
+// @homepage     https://github.com/Ocyss
 // @match        https://leetcode.cn/problems/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=leetcode.cn
 // @license      MIT
